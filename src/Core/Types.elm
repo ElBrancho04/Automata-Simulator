720521876808c3f682dd5b768103dbbe92327792
ElBrancho04 <maharbazehcnas@gmail.com>
2025-12-18 23:05:57 -0500

    Initial commit

---
+++ src/Core/Types.elm
@@ -0,0 +1,28 @@
+module Core.Types exposing (..)
+
+type Cell = Alive | Dead
+type alias Grid = List (List Cell)
+type alias Rules = { birth : List Int, survive : List Int }
+type alias Position = (Int, Int)
+
+type Page = ConfigPage | SimulationPage
+
+type alias ConfigState =
+    { grid : Grid
+    , rules : Rules
+    , width : Int
+    , height : Int
+    }
+
+type alias SimulationState =
+    { grid : Grid
+    , rules : Rules
+    , generation : Int
+    , isPlaying : Bool
+    }
+
+type alias AppState =
+    { currentPage : Page
+    , configState : ConfigState
+    , simulationState : SimulationState
+    }
